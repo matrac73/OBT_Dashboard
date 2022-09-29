d52e931414fd5d57a3433f141e9f00c117f39de2
mathieu.ract <mathieu.ract@student-cs.fr>
2022-09-29 09:30:11 +0200

    start on the login page

--- OBT_Dashboard_frontend/src/index.tsx
+++ OBT_Dashboard_frontend/src/index.tsx
@@ -9,11 +9,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<App />}>
+      <Route path="/" element={<App />}>
         <Route path="/" element={<Dashboard />} />
       </Route>
       <Route path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root/login')
 );
